0f0d3508a0a5c3b19e7fb1894d3ab6fa6d36d2c6
Anjugadwal84 <Anju_gadwal84@yahoo.com>
2020-10-10 18:12:19 +0530

    Daily Practise Folder

---
+++ SimplePrograms/SimplePractise.java
@@ -0,0 +1,9 @@
+class SimplePractise {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        for(String arg : args){
+            System.out.println(arg);
+        }
+    }
+    }
+   
